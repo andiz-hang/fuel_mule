678495d930a1c8cb7e459c30ef55db158ee5fd88
andiz <andiz@sfu.ca>
2021-03-26 18:05:44 -0700

    Second commit

--- app/src/main/java/com/example/fuel_mule/LoginActivity.java
+++ app/src/main/java/com/example/fuel_mule/LoginActivity.java
@@ -1,13 +1,32 @@
 package com.example.fuel_mule;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private Button mLoginButton;
+    private String mUsername;
+//    private String mPassword; #NOT NEEDED FOR THE DEMO
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mLoginButton = findViewById(R.id.login_button);
+        mLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                mUsername = ;
+
+                Intent intent = MainActivity.newIntent(LoginActivity.this, mUsername);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/fuel_mule/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.fuel_mule;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String EXTRA_USERNAME = "com.example.fuel_mule.username";
+
+    public static Intent newIntent(Context packageContext, String username) {
+        Intent intent = new Intent(packageContext, MainActivity.class);
+        intent.putExtra(EXTRA_USERNAME, username);
+        return intent;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
