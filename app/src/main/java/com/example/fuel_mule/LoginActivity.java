34f2f508cf547626512e6c99ce877e7e348afb0f
andiz <andiz@sfu.ca>
2021-03-26 18:59:05 -0700

    Added Username login details

--- app/src/main/java/com/example/fuel_mule/LoginActivity.java
+++ app/src/main/java/com/example/fuel_mule/LoginActivity.java
@@ -3,12 +3,16 @@ package com.example.fuel_mule;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
 
     private Button mLoginButton;
+    private EditText mUsernameField;
     private String mUsername;
 //    private String mPassword; #NOT NEEDED FOR THE DEMO
 
@@ -17,12 +21,29 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        mUsernameField = findViewById(R.id.username_textbox);
+        mUsernameField.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+              // This space intentionally left blank
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+              mUsername = s.toString();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+              // This one too
+            }
+        });
+
         mLoginButton = findViewById(R.id.login_button);
         mLoginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                mUsername = ;
 
                 Intent intent = MainActivity.newIntent(LoginActivity.this, mUsername);
                 startActivity(intent);
