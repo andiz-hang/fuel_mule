17709d3a38078c91742fab221302098df5d9e1fc
andiz <andiz@sfu.ca>
2021-04-08 16:21:13 -0700

    Added Meal class

---
+++ app/src/main/java/com/example/fuel_mule/Meal.java
@@ -0,0 +1,41 @@
+package com.example.fuel_mule;
+
+import java.util.List;
+
+public class Meal {
+    private String mName;
+
+    private int mCalories;
+    private int mSodium;
+    private int mFat;
+    private int mCarbs;
+    private int mProtein;
+
+    public Meal(String name) {
+        mName = name;
+
+        mCalories = 0;
+        mSodium = 0;
+        mFat = 0;
+        mCarbs = 0;
+        mProtein = 0;
+    }
+
+    public Meal(String name, List<Integer> nutrition) {
+        mName = name;
+
+        mCalories = nutrition.get(0);
+        mSodium = nutrition.get(1);
+        mFat = nutrition.get(2);
+        mCarbs = nutrition.get(3);
+        mProtein = nutrition.get(4);
+    }
+
+    public void add(Meal m) {
+        mCalories += m.mCalories;
+        mSodium += m.mSodium;
+        mFat += m.mFat;
+        mCarbs += m.mCarbs;
+        mProtein += m.mProtein;
+    }
+}
